896c0e5f617767e84d94240fb0b8f0ee3b77194c
Łukasz Zygmański <lzygmanski@nayment.pl>
2020-01-19 02:11:35 +0100

    Add scripts

---
+++ scripts/source.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+
+
+# BSPWM
+cd $HOME
+rm -Rf bspwm
+git clone https://github.com/baskerville/bspwm.git
+cd bspwm && make && sudo make install
+
+# SXHKD
+cd $HOME
+rm -rf sxhkd
+git clone https://github.com/baskerville/sxhkd.git
+cd sxhkd && make && sudo make install
---
+++ scripts/ubuntu.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+MY_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
+
+## Lib
+sudo apt-get install \
+	libxcb-xinerama0-dev \
+	libxcb-icccm4-dev \
+	libxcb-randr0-dev libxcb-util0-dev \
+	libxcb-ewmh-dev libxcb-keysyms1-dev \
+	libxcb-shape0-dev
+
+## Standard
+sudo apt-get install \
+	neovim \
+	vim \
+	tmux \
+ 	git \
+	autocutsel \
+	compton \
+	feh \
+	stow
+
+## From Source
+sh $MY_DIR/source.sh
+
+## Links
+cd $MY_DIR/..
+stow -v -R -t $HOME \
+	bspwm \
+	compton \
+	neovim \
+	sxhkd \
+	tmux \
+	vim \
+	xorg
