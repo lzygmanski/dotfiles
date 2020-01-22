83a53b379262777f219b64d9af93410fd5c8c8c9
Łukasz Zygmański <lzygmanski@nayment.pl>
2020-01-22 21:34:19 +0100

    Add brave

--- scripts/ubuntu/install.sh
+++ scripts/ubuntu/install.sh
@@ -71,3 +71,11 @@ sudo snap install --classic slack
 ### Chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
+
+## Other
+### Brave install
+sudo apt install apt-transport-https curl
+curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
+echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo apt update
+sudo apt install brave-browser
