d5013819a9f17c267552d03b77cf112cc1fef965
Lukasz Zygmanski <lzygmanski@gmail.com>
2020-11-09 09:29:30 +0100

    refactor: scripts updates

--- scripts/general/source-base.sh
+++ scripts/general/source-base.sh
@@ -2,11 +2,14 @@
 
 cd $HOME
 
-## PLUG 
+## ohmyzsh
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+## plug 
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-## TMP
+## tpm
 cd $HOME
 sudo rm -rf .tmux
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
--- scripts/macos/dracula-theme.sh
+++ scripts/macos/dracula-theme.sh
@@ -4,6 +4,9 @@
 mkdir -p ~/.theme/dracula
 cd ~/.theme/dracula
 
+sudo rm -rf iterm 
 git clone https://github.com/dracula/iterm.git
+
+sudo rm -rf terminal-app
 git clone https://github.com/dracula/terminal-app.git
 
